aa33b911db1e49d799ee7d40fd99f1408879e422
Chintan Suthar <chintandineshkumar.suthar@my.jcu.edu.au>
2019-09-14 21:44:21 +1000

    theme header bootstrap commit

---
+++ themes/startuptheme/footer.php
@@ -0,0 +1,11 @@
+</div><!-- /.row -->
+
+</div><!-- /.container -->
+
+<footer class="blog-footer">
+    <p>u3atownsville library <a href="#">u3atownsville.com</a> by <a href="#">team_007</a>.</p>
+    <p><a href="#">Back to top</a></p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ themes/startuptheme/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="blog-nav">
+            <a class="blog-nav-item active" href="#">Home</a>
+            <a class="blog-nav-item" href="#">About Us</a>
+            <a class="blog-nav-item" href="#">Classes</a>
+            <a class="blog-nav-item" href="#">JCU Classes</a>
+            <a class="blog-nav-item" href="#">News</a>
+            <a class="blog-nav-item" href="#">Events</a>
+            <a class="blog-nav-item" href="#">Contact Us</a>
+        </nav>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="blog-header">
+        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+    </div>
+
+    <div class="row">
